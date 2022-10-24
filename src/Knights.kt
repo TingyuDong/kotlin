c157269b5bf23db5c8cd08b8d7a3a9ddc5eb84dd
Tingyu Dong <tingyu.dong@thoughtworks.com>
2022-10-24 20:52:27 +0800

    fix:delete redundant code

--- src/Knights.kt
+++ src/Knights.kt
@@ -1,11 +1,11 @@
-fun main(args: Array<String>) {
+fun main() {
     val solution = Solution()
     println(solution.knightProbability(3, 2, 0, 0))
 }
 
 class Solution {
     fun knightProbability(n: Int, k: Int, row: Int, column: Int): Double {
-        var possibleDirection = arrayOf(
+        val possibleDirection = arrayOf(
                 arrayOf(-1, 2),
                 arrayOf(1, 2),
                 arrayOf(2, 1),
@@ -15,7 +15,7 @@ class Solution {
                 arrayOf(-2, -1),
                 arrayOf(-2, 1)
         )
-        var currentMatrix = Array(k + 1) { Array(n) { DoubleArray(n) { 0.0 } } }
+        val currentMatrix = Array(k + 1) { Array(n) { DoubleArray(n) { 0.0 } } }
 
         for (step in 0..k) {
             for (i in 0 until n) {
@@ -24,8 +24,8 @@ class Solution {
                         currentMatrix[step][i][j] = 1.0
                     } else {
                         for (move in possibleDirection.indices) {
-                            var nextI = i + possibleDirection[move][0]
-                            var nextJ = j + possibleDirection[move][1]
+                            val nextI = i + possibleDirection[move][0]
+                            val nextJ = j + possibleDirection[move][1]
                             if (nextI in 0 until n && nextJ in 0 until n) {
                                 currentMatrix[step][i][j] += currentMatrix[step - 1][nextI][nextJ] / 8
                             }
